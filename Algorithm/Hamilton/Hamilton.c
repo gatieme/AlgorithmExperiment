7ed88b8228760c97106e2d178fc5c0d689387469
gatieme <gatieme@163.com>
2015-11-30 15:21:05 +0800

    提交哈密顿图的信息

--- Algorithm/Hamilton/Hamilton.c
+++ Algorithm/Hamilton/Hamilton.c
@@ -1,21 +1,17 @@
-//
-//http://blog.csdn.net/Wiking__acm/article/details/12709023void Hamilton()
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
 
-
-#define maxn 200
+#include <sys/time.h>
+#define MAX_SIZE 200
 #define INF 1000000000
 
 int N, M;
-bool graph[maxn][maxn];
-int hami[maxn];            //记录回路中的第i个点
-bool flag[maxn];
+bool graph[MAX_SIZE][MAX_SIZE];
+int hami[MAX_SIZE];            //记录回路中的第i个点
+bool flag[MAX_SIZE];
 
 
 
@@ -23,10 +19,18 @@ bool flag[maxn];
 void Hamilton();
 //  打印哈密顿回路
 void PrintHamilton();
-
+//  打印图的信息
+void PrintGraph();
 int main()
 {
+#ifdef DEBUG
     freopen("in.txt", "r", stdin);
+#endif
+
+#ifdef RELEASE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 
     while(scanf("%d%d", &N, &M) != EOF)
     {
@@ -43,9 +47,31 @@ int main()
             b--;
             graph[a][b] = graph[b][a] = true;
         }
+#ifdef DEBUG
+        printf("=================START=================\n");
+	    PrintGraph();
+#endif
 
+       //  计算运行时间
+//ifdef TIME
+        struct timeval start, end;
+        gettimeofday( &start, NULL );
+//#endif // TIME
+        //  图graph的哈密顿环
         Hamilton();
+
+
+	    gettimeofday( &end, NULL );
+        int timeuse = 1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec -start.tv_usec;
+
+#ifdef DEBUG
         PrintHamilton();
+        printf("\nN = %5d, usetime = %10dus\n\n", N, timeuse);
+        printf("==================END==================\n\n\n");
+#endif
+#ifdef RELEASE
+        printf("%5d%10d\n", N, timeuse);
+#endif
     }
 
     return EXIT_SUCCESS;
@@ -55,6 +81,9 @@ int main()
 
 void PrintHamilton()
 {
+#ifdef DEBUG
+    printf("\nThe all hamilton of the garph\n");
+#endif
     bool f = false;
     for(int i = 0; i < N; i++)
     {
@@ -73,15 +102,15 @@ void PrintHamilton()
     }
     else
     {
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i <= N; i++)
         {
-            if(i == N - 1)
+            if(i == N)
             {
-                printf("%d\n", hami[i] + 1);
+                printf("%2d\n", hami[0] + 1);
             }
             else
             {
-                printf("%d ", hami[i] + 1);
+                printf("%2d -=>", hami[i] + 1);
             }
         }
     }
@@ -91,18 +120,18 @@ void PrintHamilton()
 void Hamilton()
 {
     int k;
-    bool s[maxn];
+    bool visited[MAX_SIZE];
 
     //  初始化设置图中没有哈密顿环
     for(int i = 0; i < N; i++)
     {
         hami[i] = -1;
-        s[i] = false;
+        visited[i] = false;
     }
 
     // 从第一个点出发开始寻找
     k = 1;
-    s[0] = true;
+    visited[0] = true;
     hami[0] = 0;
 
     while(k >= 0)
@@ -111,7 +140,7 @@ void Hamilton()
 
         while(hami[k] < N)                      //  走到最后一个定点为止
         {
-            if(s[hami[k]] != true
+            if(visited[hami[k]] != true
             && graph[hami[k - 1]][hami[k]])     //  从k-1到当前顶点有路可达
             {
                 break;
@@ -125,7 +154,7 @@ void Hamilton()
         if((hami[k] < N)
         && (k != N - 1))
         {
-            s[hami[k++]] = true;
+            visited[hami[k++]] = true;
         }
         else if((hami[k] < N)
              && (k == N - 1)
@@ -137,8 +166,28 @@ void Hamilton()
         {
             hami[k] = -1;
             k--;
-            s[hami[k]] = false;
+            visited[hami[k]] = false;
         }
     }
+}
+
+
+
+// 输出图的信息
+void PrintGraph(/*int (*graph)[MAX_SIZE], int n*/)
+{
+	printf("There are %d points and %d vertexs\n", N, M);
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+#if 0                               //  也可以使用输入矩阵的方式输入图的信息
+                scanf("%d", &a);
+                graph[i][j] = a;
+#endif
+			printf("%3d", graph[i][j]);
+		}
+		printf("\n");
+	}
 }
 
